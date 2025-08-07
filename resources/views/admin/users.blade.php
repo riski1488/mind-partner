2e5c8818d5012715f101f2900f089dcefea41e08
riski1488 <riski69420@gmail.com>
2025-08-07 22:00:00 +0900

    Update users table view to include Edit and Delete buttons

---
+++ resources/views/admin/users.blade.php
@@ -0,0 +1,24 @@
+<!-- Add Edit and Delete buttons to the user table -->
+<table>
+    <thead>
+        <tr>
+            <th>User</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($users as $user)
+            <tr>
+                <td>{{ $user->name }}</td>
+                <td>
+                    <a href="{{ route('admin.users.edit', $user->id) }}">Edit</a>
+                    <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
